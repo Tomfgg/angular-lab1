58c676c6ca96965652becc3fa090b0cb56aad173
zain <omarzain326@gmail.com>
2024-01-15 15:38:30 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortComponent } from './port/port.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeroComponent, AboutComponent, SkillsComponent, PortComponent, ContactComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
